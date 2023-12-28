d2a4a6e27c1723f6b75e382b3f01ce0c625df9f0
Flootersmm <conrad.clough@hotmail.com>
2023-12-29 00:44:12 +0100

    Added custom brightness_control.sh

---
+++ private_dot_config/private_sway/executable_brightness_control.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Function to decrease brightness
+decrease_brightness() {
+    current_brightness=$(xbacklight -get | cut -d '.' -f 1)
+    if [ "$current_brightness" -le 5 ]; then
+        if [ "$current_brightness" -gt 1 ]; then
+            xbacklight -set 1
+        else
+            xbacklight -set 0
+        fi
+    else
+        xbacklight -dec 5
+    fi
+}
+
+# Function to increase brightness
+increase_brightness() {
+    current_brightness=$(xbacklight -get | cut -d '.' -f 1)
+    if [ "$current_brightness" -eq 0 ]; then
+        xbacklight -set 1
+    elif [ "$current_brightness" -lt 5 ]; then
+        xbacklight -set 5
+    else
+        xbacklight -inc 5
+    fi
+}
+
+# Main
+case "$1" in
+    up)
+        increase_brightness
+        ;;
+    down)
+        decrease_brightness
+        ;;
+    *)
+        echo "Usage: $0 {up|down}"
+        exit 1
+        ;;
+esac
+
