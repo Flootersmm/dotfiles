824427e0dd33eec156aeb5c9a9d9e5e97bdc3b35
Flootersmm <conrad.clough@hotmail.com>
2024-01-15 17:30:31 +0100

    Added wallpaper.sh

--- private_dot_config/nvim/lua/custom/chadrc.lua
+++ private_dot_config/nvim/lua/custom/chadrc.lua
@@ -3,4 +3,8 @@
  M.ui = { theme = 'chadracula' }
  M.plugins = "custom.plugins"
  M.mappings = require("custom.mappings")
+
+vim.api.nvim_set_hl(0, "Comment", { fg = "#999999"})
+vim.api.nvim_set_hl(0, "@comment", { link = "Comment"})
+
  return M
