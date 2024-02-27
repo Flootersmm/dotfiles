c49504d3826ef88b2bf8c9b650526c3f43adb2ba
Flootersmm <conrad.clough@hotmail.com>
2024-02-27 11:03:27 +0100

    Added brightness_toggle.sh

---
+++ private_dot_config/private_sway/executable_brightness_toggle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Function to set brightness to 1% or 100%, toggling between extremes
+toggle_brightness() {
+    current_brightness=$(xbacklight -get | cut -d '.' -f 1)
+    if [ "$current_brightness" -eq 1 ]; then
+        xbacklight -set 100
+    elif [ "$current_brightness" -eq 100 ]; then
+        xbacklight -set 1
+    else
+        if [ "$current_brightness" -le 50 ]; then
+            xbacklight -set 1
+        else
+            xbacklight -set 100
+        fi
+    fi
+}
+
+toggle_brightness
+
